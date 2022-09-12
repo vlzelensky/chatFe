8621ed92003ca67fd1a9bc776665bf610e8614ef
vlzelensky <vzelensky.exceedteam@gmail.com>
2022-09-12 10:29:01 +0300

    initial commit

--- src/pages/SignIn/SignIn.tsx
+++ src/pages/SignIn/SignIn.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 
-const App = () => {
+export const SignIn = () => {
   return <div></div>;
 };
-
-export default App;
--- src/pages/SignUp/SignUp.tsx
+++ src/pages/SignUp/SignUp.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 
-const App = () => {
+export const SignUp = () => {
   return <div></div>;
 };
-
-export default App;
