94127cfeb1c1e4791085a567e4af7426d17af8e0
vlzelensky <vzelensky.exceedteam@gmail.com>
2022-09-19 12:20:16 +0300

    feat/fix pr

--- src/api/types.ts
+++ src/api/types.ts
@@ -10,7 +10,7 @@ export interface RegisterUserDataI {
   name: string;
   userName: string;
   password: string;
-  repeatPassword: string;
+  repeatPassword?: string;
   birthDate: string;
   email: string;
 }
