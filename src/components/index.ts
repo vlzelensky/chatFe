94be8813a9a881518a5efb0f6cc630bc2540b229
vlzelensky <vzelensky.exceedteam@gmail.com>
2022-09-19 18:25:53 +0300

    feat/create main layout/ users search

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,3 @@
 export * from './Form';
+export * from './Layout';
+export * from './Search';
--- src/store/user.ts
+++ src/store/user.ts
@@ -1,31 +1,48 @@
 import { makeAutoObservable } from 'mobx';
-import { logIn, getUser } from 'api';
+import { logIn, getUser, findUser } from 'api';
 import { initApi } from 'config/axios';
+import { LoginResponse } from 'api/types';
 
 class User {
   constructor() {
     makeAutoObservable(this);
   }
 
+  id: string | null = null;
   isAuth: boolean = false;
   email: string | null = null;
+  avatar: string | null = null;
   name: string | null = null;
   userName: string | null = null;
   birthDate: string | null = null;
 
+  private setUser(data: LoginResponse) {
+    this.isAuth = true;
+    this.id = data!.id;
+    this.email = data!.email;
+    this.name = data!.name;
+    this.userName = data!.userName;
+    this.birthDate = data!.birthDate;
+    initApi(data!.token);
+  }
+
   async signIn(userData: { password: string; email: string }) {
     try {
       const data = await logIn(userData);
-      this.isAuth = true;
-      this.email = data!.email;
-      this.name = data!.name;
-      this.userName = data!.userName;
-      this.birthDate = data!.birthDate;
+      this.setUser(data);
       localStorage.setItem('token', data!.token);
-      initApi(data!.token);
     } catch {
       return 'Неправильный логин или пароль';
     }
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    this.isAuth = false;
+    this.email = null;
+    this.name = null;
+    this.userName = null;
+    this.birthDate = null;
   }
 
   async getUser() {
@@ -33,21 +50,17 @@ class User {
     if (token) {
       try {
         const data = await getUser(token);
-        this.isAuth = true;
-        this.email = data!.email;
-        this.name = data!.name;
-        this.userName = data!.userName;
-        this.birthDate = data!.birthDate;
-        initApi(data!.token);
+        this.setUser(data);
       } catch (e) {
-        localStorage.removeItem('token');
-        this.isAuth = false;
-        this.email = null;
-        this.name = null;
-        this.userName = null;
-        this.birthDate = null;
+        this.logout();
       }
     }
+  }
+
+  async searchUser(value: string, userId: string) {
+    try {
+      const res = await findUser(value, userId);
+    } catch (e) {}
   }
 }
 
